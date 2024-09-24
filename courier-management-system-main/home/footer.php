8da8652a07710cea1d5b69a189c5c2592e084856
Suhas C <11suhassuhas@gmail.com>
2024-09-25 00:27:59 +0530

    Add files via upload

---
+++ courier-management-system-main/home/footer.php
@@ -0,0 +1,25 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+   
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px">      </p><br>
+  <a href="mailto:vinayksgowda@gmail.com" style="color:brown">  </a>
+
+</footer>
+
+</body>
+</html>
